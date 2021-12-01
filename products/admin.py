b494eb574fb0c6a4a44ed3456f3b9461ba6432cd
unknown <jackosullivan541@gmail.com>
2021-12-01 12:05:16 +0000

    readjust admin display for products app models

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,38 @@ from django.contrib import admin
 from .models import Product, Category, WiderCategory
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(WiderCategory)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "author",
+        "category",
+        "model",
+        "description",
+        "price",
+        "rating",
+        "book_of_month",
+        "currently_on_sale",
+        "image",
+        "pdf",
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "friendly_name",
+    )
+
+
+class WiderCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "friendly_name",
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(WiderCategory, WiderCategoryAdmin)
