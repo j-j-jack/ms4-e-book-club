429e3c27c32ca4e05353ad9cab06534e97bcc7bd
unknown <jackosullivan541@gmail.com>
2021-12-01 13:50:06 +0000

    create fiction view for products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
+from django.contrib import admin
+from django.shortcuts import render
+from .models import Product
+# Create your views here.
+
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def fiction(request):
+    """ A view to return the index page """
+
+    products = Product.objects.all()
+    context = {
+        "products": products
+    }
+    return render(request, 'products/fiction.html', context)
