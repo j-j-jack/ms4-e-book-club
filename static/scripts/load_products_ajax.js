5d02c6121b2aa338b3a9d7c0d134c74680d55019
unknown <jackosullivan541@gmail.com>
2021-12-03 20:08:20 +0000

    add error handling to load products ajax response

--- static/scripts/load_products_ajax.js
+++ static/scripts/load_products_ajax.js
@@ -3,8 +3,6 @@ $(document).ready(function () {
   let loadRound = 0;
   let allCategories = jQuery("#all-categories").text();
   allCategories = JSON.parse(allCategories);
-  console.log(allCategories);
-  let unknownTitleTemplate = ``;
   jQuery("#load-more").click(function () {
     loadRound += 1;
     jQuery.ajax({
@@ -23,22 +21,33 @@ $(document).ready(function () {
           let bookCategoryFriendlyName;
           let bookId;
           jsonResponse = JSON.parse(response.items);
-          console.log(jsonResponse);
-          let normaltemplate = `
+          let template = `
             <div class="row">
             <div class="product-container col-10 offset-1">
                 <div class="row mt-1 mb-2"></div>
                 <div class="row">`;
+
           for (var item in jsonResponse) {
-            console.log(item);
+            let errorExists = false;
+
             try {
               bookName = jsonResponse[item].fields.name;
             } catch (err) {
               console.log(err.message);
             }
             if (bookName == undefined || bookName == null) {
+              errorExists = true;
             }
-            bookAuthor = jsonResponse[item].fields.author;
+
+            try {
+              bookAuthor = jsonResponse[item].fields.author;
+            } catch (err) {
+              console.log(err.message);
+            }
+            if (bookAuthor == undefined || bookAuthor == null) {
+              errorExists = true;
+            }
+
             try {
               bookImage = jsonResponse[item].fields.image;
             } catch (err) {
@@ -47,19 +56,25 @@ $(document).ready(function () {
             if (bookImage == undefined || bookImage == null) {
               bookImage = "images/noimage.png";
             }
+
             try {
               bookRating = jsonResponse[item].fields.rating;
             } catch (err) {
               console.log(err.message);
             }
-            if (bookRating == undefined || bookRating == null) {
+            if (
+              bookRating == undefined ||
+              bookRating == null ||
+              bookRating == ""
+            ) {
               bookRating = "no rating";
             }
+
             try {
               categoryNumber = jsonResponse[item].fields.category;
               for (var category in allCategories) {
                 if (allCategories[category].pk == categoryNumber) {
-                  bookCategoryName = allCategories[category].fields.name;
+                  bookCategoryName = allCategories[category].fields.nam;
                   bookCategoryFriendlyName =
                     allCategories[category].fields.friendly_name;
                 }
@@ -67,10 +82,27 @@ $(document).ready(function () {
             } catch (err) {
               console.log(err.message);
             }
-            bookPrice = jsonResponse[item].fields.price;
-            bookId = jsonResponse[item].pk;
-            console.log(bookId);
-            normaltemplate += `
+
+            try {
+              bookPrice = jsonResponse[item].fields.price;
+            } catch (err) {
+              console.log(err.message);
+            }
+            if (bookPrice == undefined || bookPrice == null) {
+              errorExists = true;
+            }
+
+            try {
+              bookId = jsonResponse[item].pk;
+            } catch (err) {
+              console.log(err.message);
+            }
+            if (bookId == undefined || bookId == null) {
+              errorExists = true;
+            }
+
+            if (errorExists == false) {
+              template += `
                         <div class="col-sm-6 col-md-6 col-lg-4 col-xl-3">
                             <div class="card h-100 border-0">
                                 <a href="product_detail/${bookId}">
@@ -83,49 +115,79 @@ $(document).ready(function () {
                                     <div class="row">
                                         <div class="col">
                                             <p class="lead mb-0 text-left font-weight-bold">€${bookPrice}</p>
-                                            <p class="small mt-1 mb-0">
-                                                <a class="text-muted" href="?category=${bookCategoryName}">
+                                            <p class="small mt-1 mb-0">`;
+              if (
+                bookCategoryName != undefined ||
+                (bookCategoryName != null &&
+                  bookCategoryFriendlyName != undefined) ||
+                bookCategoryFriendlyName != null
+              ) {
+                template += `<a class="text-muted" href="?category=${bookCategoryName}">
                                                     <i class="fas fa-tag mr-1"></i>${bookCategoryFriendlyName}
                                                 </a>
-                                            </p>
+                                                `;
+              } else {
+                template += `<p>
+                                                <i class="fas fa-tag mr-1"></i>uncategorised
+                                                </p>"
+                                                `;
+              }
+              template += `</p>
                                             ${bookRating}${item}
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>`;
+            } else {
+              template += `
+                        <div class="col-sm-6 col-md-6 col-lg-4 col-xl-3">
+                            <div class="card h-100 border-0">
+                                    <img class="card-img-top img-fluid" src="/media/images/noimage.png" alt="title not found">
+                                <div class="card-body pb-0">
+                                    <p class="mb-0">Title not found</p>
+                                </div>
+                                <div class="card-footer bg-white pt-0 border-0 text-left">
+                                </div>
+                            </div>
+                        </div>`;
+            }
             if ((item + 1) % 1 == 0) {
-              normaltemplate += `
+              template += `
                             <div class="col-12 d-sm-none mb-5">
                                 <hr>
                             </div>`;
             }
             if ((item + 1) % 2 == 0) {
-              normaltemplate += `
+              template += `
                             <div class="col-12 d-none d-sm-block d-md-block d-lg-none mb-5">
                                 <hr>
                             </div>`;
             }
             if ((item + 1) % 3 == 0) {
-              normaltemplate += `
+              template += `
                             <div class="col-12 d-none d-lg-block d-xl-none mb-5">
                                 <hr>
                             </div>`;
             }
             if ((item + 1) % 4 == 0) {
-              normaltemplate += `
+              template += `
                             <div class="col-12 d-none d-xl-block mb-5">
                                 <hr>
                             </div>`;
             }
           }
-          normaltemplate += `
+          template += `
             </div>
             </div>
             </div>`;
-          jQuery("#ajax-response").append(normaltemplate);
+          jQuery("#ajax-response").append(template);
         } catch (err) {
           console.log(err.message);
+        }
+        jsonResponseLength = Object.keys(jsonResponse).length;
+        if (jsonResponseLength < 3) {
+          jQuery("#load-more-container").attr("style", " display:none");
         }
       },
     });
