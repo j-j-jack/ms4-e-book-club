3acda053b3b59154736c8759c13c978073d556f8
unknown <jackosullivan541@gmail.com>
2021-12-05 16:25:40 +0000

    modify bag contexts to return bag total and items

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,15 @@ def view_bag(request):
     """ A view to return the index page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+    bag[item_id] = 1
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
