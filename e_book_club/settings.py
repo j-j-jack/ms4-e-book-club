6a0b3618fa56478e0247d4281ef3797e6808cae6
unknown <jackosullivan541@gmail.com>
2021-12-07 16:51:41 +0000

    generate new secret key and remove from visible code

--- e_book_club/settings.py
+++ e_book_club/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gzdyf&q8sz4(_9n75ra%cne@!6o)(l#7&nyj8ok=3f_p*9l5rn'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
