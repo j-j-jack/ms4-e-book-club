b87d3e21217557c7dbb5072fdaa14a3988ac562c
unknown <jackosullivan541@gmail.com>
2021-12-08 17:08:13 +0000

    create checkout forms and views

--- e_book_club/settings.py
+++ e_book_club/settings.py
@@ -49,6 +49,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'e_book_club.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,6 +86,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': {
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            }
         },
     },
 ]
