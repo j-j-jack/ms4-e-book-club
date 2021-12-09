9a9851d0400a9317b492a3773e806d3cd6f5600c
unknown <jackosullivan541@gmail.com>
2021-12-09 18:16:26 +0000

    use developement to control which db app uses

--- e_book_club/settings.py
+++ e_book_club/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
-
+DEVELOPEMENT = os.environ.get('DEVELOPEMENT', '')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
@@ -120,15 +120,19 @@ WSGI_APPLICATION = 'e_book_club.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEVELOPEMENT:
+    print('developement version')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-# DATABASES = {
-# 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
+else:
+    print('live version')
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
