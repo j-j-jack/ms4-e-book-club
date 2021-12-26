2370ce542a2698a4811841ea6d7ae82c92144519
unknown <jackosullivan541@gmail.com>
2021-12-26 19:42:31 +0000

    replace placeholder latest books on home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    products = Product.objects.all()[0: 4]
+    context = {
+        "products": products,
+    }
+    return render(request, 'home/index.html', context)
